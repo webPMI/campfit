9ae7604bdf2820071e99f66a053aef5feb2c89be
webPMI <servicioweb.pmi@gmail.com>
2026-07-26 00:50:18 +0200

    fix: clean astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
-// https://astro.build/config
 export default defineConfig({
-<<<<<<< HEAD
-    output: 'static',
-    vite: {
-        server: {
-            headers: {
-                'Cross-Origin-Opener-Policy': 'unsafe-none',
-                'Cross-Origin-Embedder-Policy': 'unsafe-none',
-            },
-        },
-        plugins: [tailwindcss()],
-        css: {
-            transformer: 'lightningcss',
-        },
-    },
-=======
   output: 'static',
   vite: {
     plugins: [tailwindcss()],
@@ -28,6 +12,14 @@ export default defineConfig({
         '@': path.resolve('./src'),
       },
     },
+    server: {
+      headers: {
+        'Cross-Origin-Opener-Policy': 'unsafe-none',
+        'Cross-Origin-Embedder-Policy': 'unsafe-none',
+      },
+    },
+    css: {
+      transformer: 'lightningcss',
+    },
   },
->>>>>>> 4042d86ac520c28484786564a781e3d6e901af5a
 });
